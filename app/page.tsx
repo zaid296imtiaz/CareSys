63d263f13f313c9c897d7f95329b22d2685cc361
Zaid Imtiaz <zaid296imtiaz@gmail.com>
2024-07-16 22:19:14 -0400

    added dark mode support in tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl underline">Home</h1>
+      <h1 className="text-3xl text-white underline">Home</h1>
+      <Button>Click</Button>
     </div>
   );
 }
